1276f6b519f53fd74b004c1863a7fd1b53ece0b3
JMitchell-NLA <jmitchell@nla.gov.au>
2020-10-07 12:57:45 +1100

    test new pipeline

---
+++ rainbow-redeploy.sh
@@ -0,0 +1,13 @@
+cd ./excalidraw
+npm run build;
+cd ..
+source ./copybuild.sh;
+echo "what commit message do you want?"
+read msg
+git add -A;
+git commit -a -m "$msg";
+git push;
+ssh root@kooks-does.net cd /cherry/excalidrawStore && git pull
+echo "all done!"
+
+
